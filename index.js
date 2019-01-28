5be3d2f6e8134651b28f8396ed8a8648632ebb51
Miray Talal Bko <meeray.k93@gmail.com>
2019-01-28 20:56:02 +0000

    Done.

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  const token = '178e163429470240f30d4dda555fc4fe0b4f9b8a';
+
+fetch('https://api.github.com/MirayT/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
 }
 
 function getToken() {
@@ -20,3 +29,18 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+const token = '178e163429470240f30d4dda555fc4fe0b4f9b8a';
+const postData = {
+  body: 'Great stuff'
+};
+
+fetch(
+  'https://api.github.com/repos/:your_ghname/:your_repo/commits/:sha/comments',
+  {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }
+).then(res => console.log(res));
